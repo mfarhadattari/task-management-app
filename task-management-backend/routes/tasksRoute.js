4f7ea94787575e5d49ae661aaea9b1011ea81013
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-08-04 15:57:51 +0600

    Delete task api implemented

--- task-management-backend/routes/tasksRoute.js
+++ task-management-backend/routes/tasksRoute.js
@@ -12,4 +12,17 @@ router.get("/tasks", async (req, res) => {
   }
 });
 
+// Delete a task api
+router.delete("/delete-task/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json({ message: "Task deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
